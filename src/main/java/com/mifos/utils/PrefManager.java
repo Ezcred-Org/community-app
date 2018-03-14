e05308b31f5b783cb02deb25e4c16b0122612618
Anish Chopra <anish@ezcred.in>
2018-03-14 13:09:57 +0530

    AGENTAPP-343 | minor changes

--- src/main/java/com/mifos/utils/PrefManager.java
+++ src/main/java/com/mifos/utils/PrefManager.java
@@ -217,6 +217,7 @@ public class PrefManager {
         return getString(EZCRED_LAST_APP_VERSION_NAME, "");
     }
 
+
     public void setEzcredLastAppVersionName(String versionName) {
         putString(EZCRED_LAST_APP_VERSION_NAME, versionName);
     }
