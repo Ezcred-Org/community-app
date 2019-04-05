b001c181cd8178d7038332579b894abdffc6e5fb
anishchopra1 <31067505+anishchopra1@users.noreply.github.com>
2019-04-05 14:52:28 +0530

    CREDIT-2819|updating ClientIdentifierCheckResponse body (#72)
    * CREDIT-2819|updating ClientIdentifierCheckResponse body
    
    * CREDIT-2819|Updating ClientIdentifierCheckResponse body

--- src/main/java/com/mifos/objects/client/ClientIdentifierCheckResponse.java
+++ src/main/java/com/mifos/objects/client/ClientIdentifierCheckResponse.java
@@ -4,5 +4,11 @@ import lombok.Data;
 
 @Data
 public class ClientIdentifierCheckResponse {
-  private final long clientId;
+
+  private final boolean success;
+  private final Long clientId;
+  private final String userMessage;
+
+  private final boolean allowUpdatePan;
+  private final boolean allowUpdatePhoneNumber;
 }
