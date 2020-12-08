cb2c7574c339a930b4036f434218145060930eb1
Anish Chopra <anish@ezcred.in>
2020-12-08 10:38:53 +0530

    CREDIT-7219 | FormUrlEncoded data

--- src/main/java/com/mifos/api/services/OAuthService.java
+++ src/main/java/com/mifos/api/services/OAuthService.java
@@ -2,26 +2,31 @@ package com.mifos.api.services;
 
 import com.mifos.api.model.APIEndPoint;
 import com.mifos.objects.oauth.OAuthTokenResponse;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 import rx.Observable;
 
 public interface OAuthService {
 
+  @FormUrlEncoded
   @POST(APIEndPoint.TOKEN)
   Observable<OAuthTokenResponse> fetchOAuthToken(
-      @Query("username") String userName,
-      @Query("password") String password,
-      @Query("client_id") String clientId,
-      @Query("client_secret") String clientSecret,
-      @Query("grant_type") String grantType
+      @Field("username") String userName,
+      @Field("password") String password,
+      @Field("client_id") String clientId,
+      @Field("client_secret") String clientSecret,
+      @Field("grant_type") String grantType
   );
 
+  @FormUrlEncoded
   @POST(APIEndPoint.TOKEN)
   Observable<OAuthTokenResponse> refreshOAuthToken(
-      @Query("refresh_token") String refreshToken,
-      @Query("client_id") String clientId,
-      @Query("client_secret") String clientSecret,
-      @Query("grant_type") String grantType
+      @Field("refresh_token") String refreshToken,
+      @Field("client_id") String clientId,
+      @Field("client_secret") String clientSecret,
+      @Field("grant_type") String grantType
   );
 }
