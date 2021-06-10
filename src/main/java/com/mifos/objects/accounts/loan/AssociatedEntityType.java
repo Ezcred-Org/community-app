00e1de6d537ff4126c9577a577696bd7107a5eb6
Anish Chopra <anish@ezcred.in>
2021-06-11 00:16:34 +0530

    CREDIT-8008 | address verification screen

---
+++ src/main/java/com/mifos/objects/accounts/loan/AssociatedEntityType.java
@@ -0,0 +1,36 @@
+package com.mifos.objects.accounts.loan;
+
+
+public enum AssociatedEntityType {
+  SELF("Self"),
+  PARENT_LOAN("Parent"),
+  GUARANTOR("Guarantor"),
+  CO_APPLICANT("Co-Applicant"),
+  VERIFICATION("Verification");
+
+  private final String value;
+
+  AssociatedEntityType(String value) {
+    this.value = value;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  public static AssociatedEntityType convertToAssociatedEntityEnum(String associatedEntityType) {
+    try {
+      if (associatedEntityType != null) {
+        return valueOf(associatedEntityType);
+      }
+    } catch (IllegalArgumentException ignore) {
+      for (AssociatedEntityType associatedEntityTypeEnum : values()) {
+        if (associatedEntityTypeEnum.getValue().equals(associatedEntityType)) {
+          return associatedEntityTypeEnum;
+        }
+      }
+    }
+
+    return null;
+  }
+}
